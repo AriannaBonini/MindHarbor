db17b3f238ba9c326c2b5a07db5977bde4b0fb8d
Elia Cinti <58148545+EliaCinti@users.noreply.github.com>
2024-02-15 23:01:17 +0100

    Delete demo-MindHarbor directory

--- demo-MindHarbor/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.demomindharbor {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens com.example.demomindharbor to javafx.fxml;
-    exports com.example.demomindharbor.logic;
-}
