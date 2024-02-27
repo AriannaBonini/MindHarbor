65d6ab0887dbc7227d495500cf0d1428ce38f0b4
Elia Cinti <58148545+EliaCinti@users.noreply.github.com>
2024-02-27 16:23:56 +0100

    LOGIN + UI PAZIENTE -> OK

--- mind_harbor/mind_harbor/src/main/java/com/example/mind_harbor/Controller/LoginGraphicController.java
+++ mind_harbor/mind_harbor/src/main/java/com/example/mind_harbor/Controller/LoginGraphicController.java
@@ -2,10 +2,14 @@ package com.example.mind_harbor.Controller;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
 import javafx.event.ActionEvent;
+import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,9 +39,35 @@ public class LoginGraphicController {
         loginController.attemptLogin(username, password, new LoginController.LoginResulHendler() {
             @Override
             public void onLoginSuccess(String role) {
-                Platform.runLater(() -> msgLbl.setText("Benvenuto " + role + ": " + username));
+                Platform.runLater(() -> {
+                    msgLbl.setText("Benvenuto " + role + ": " + username);
+                    logger.info("Login riuscito per l'utente: {}", username);
+                    // Gestisci la navigazione in base al ruolo
+                    try {
+                        if ("Paziente".equals(role)){
+                            // l'utente è un paziente
+                            // carico l'interfaccia grafica della home del paziente
+                            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/mind_harbor/FXML/HomePaziente_NoTest.fxml"));
+                            Parent root = loader.load();
+
+                            // ora imposto la scena
+                            Stage stage = new Stage();
+                            stage.setTitle("Home Page Paziente");
+                            stage.setScene(new Scene(root));
+                            stage.show();
+
+                            // ora chiudiamo la finestra di login
+                            Stage currentStage = (Stage) msgLbl.getScene().getWindow();
+                            currentStage.close();
+                        } else if ("Psicologo".equals(role)) {
+                            // qui si fa la stessa cosa fatta sopra ma con la home dello psicologo
+
+                        }
+                    }catch (Exception e){
+                        logger.error("Errore durante il caricamento della home page", e);
+                    }
+                });
                 logger.info("Login riuscito per l'utente: {}", username);
-                // Gestisci la navigazione in base al ruolo
             }
 
             @Override
