ab4fc26b595ba44015fd435305a6dd97499d7d5e
AriannaBonini <159540935+AriannaBonini@users.noreply.github.com>
2024-03-01 01:29:51 +0100

    Delete demo-MindHarbor directory

--- demo-MindHarbor/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.demomindharbor {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens com.example.demomindharbor.logic.graphic_controllers to javafx.fxml;
-    exports com.example.demomindharbor.logic;
-    exports com.example.demomindharbor.logic.graphic_controllers to javafx.fxml;
-}
