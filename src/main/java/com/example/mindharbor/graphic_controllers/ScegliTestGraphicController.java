c5b6bd78da3a02be5549a684176f3bd6d3138779
Arianna Bonini <ariannabonini98@gmail.com>
2024-05-07 17:03:39 +0200

    Aggiornamento GUI 7/05/2024

---
+++ src/main/java/com/example/mindharbor/graphic_controllers/ScegliTestGraphicController.java
@@ -0,0 +1,14 @@
+package com.example.mindharbor.graphic_controllers;
+
+import com.example.mindharbor.app_controllers.ScegliTestController;
+
+public class ScegliTestGraphicController {
+
+    public void initialize() {
+        ScegliTestController scegliTest= new ScegliTestController();
+
+    }
+
+
+
+}
