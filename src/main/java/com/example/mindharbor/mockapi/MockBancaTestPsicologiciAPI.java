3150c00f0a39930ca6cac58c9821581c57838b4e
EliaCinti <cintielia21@gmail.com>
2024-05-09 16:16:36 +0200

    Aggiunta MockAPI

---
+++ src/main/java/com/example/mindharbor/mockapi/MockBancaTestPsicologiciAPI.java
@@ -0,0 +1,18 @@
+package com.example.mindharbor.mockapi;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+public class MockBancaTestPsicologiciAPI {
+    private static final WireMockServer wireMockServer = new WireMockServer(8080);
+    /* public static void mockTestPiscologiciAPI() {
+        wireMockServer.start();
+        stubFor(get(urlEqualTo("/test"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "text/plain")
+
+    }
+     */
+}
