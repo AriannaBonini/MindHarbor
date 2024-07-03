dc8248a2206ddc2a9f80894e39f224e89f974e97
EliaCinti <cintielia21@gmail.com>
2024-07-04 00:47:49 +0200

    "Update graphic_controller con relativi FXML - SonarLint"

---
+++ src/main/java/com/example/mindharbor/constants/Constants.java
@@ -0,0 +1,11 @@
+package com.example.mindharbor.constants;
+
+public class Constants {
+
+    private Constants(){
+        // Costruttore privato per impedire l'istanza della classe
+    }
+
+    public static final String INTERFACE_LOAD_ERROR = "Impossibile caricare l'interfaccia";
+    public static final String DELETE_REQUEST = "Errore nell'eliminazione della richiesta di appuntamento";
+}
