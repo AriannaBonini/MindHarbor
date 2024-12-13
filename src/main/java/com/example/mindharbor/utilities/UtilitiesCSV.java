9ff512a01457fff83aee2626bd65602120049f85
EliaCinti <cintielia21@gmail.com>
2024-12-13 12:29:47 +0100

    CSV OK

--- src/main/java/com/example/mindharbor/utilities/UtilitiesCSV.java
+++ src/main/java/com/example/mindharbor/utilities/UtilitiesCSV.java
@@ -11,12 +11,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.sql.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class UtilitiesCSV {
